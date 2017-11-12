6e5fe5e125b414c5a4031c34d81bba7fb0cae458
Dvir Volk <dvirsky@gmail.com>
2017-11-12 12:22:10 +0200

    version bump - 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redisearch',
-    version='0.6.3',
+    version='0.7.0',
 
     description='RedisSearch Python Client',
     url='http://github.com/RedisLabs/redisearch-py',
