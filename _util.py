315500e1fb64c921e94b75cefbaf75672628b110
Benjamin Clavie <benjaminclavie@gmail.com>
2018-03-26 14:53:38 +0200

    Improved python3-supporting _util.py import

--- _util.py
+++
@@ -1,9 +0,0 @@
-import six
-
-def to_string(s):
-    if isinstance(s, six.string_types):
-        return s
-    elif isinstance(s, bytes):
-        return s.decode('utf-8')
-    else:
-        return s  # Not a string we care about
