a8c11e85ee97441a7b76b5a9771d637d9f621201
Adam <cheeseandcereal@users.noreply.github.com>
2019-10-27 02:36:07 -0700

    feat: Add 'alter schema add' support (#38)
    * add alter schema add support
    * do gendoc update

--- test/test.py
+++ test/test.py
@@ -494,6 +494,31 @@ class RedisSearchTestCase(ModuleTestCase('../module.so')):
                 res = client.search(q)
                 self.assertEqual(1, res.total)
 
+    def testAlterSchemaAdd(self):
+        conn = self.redis()
+
+        with conn as r:
+            # Creating a client with a given index name
+            client = Client('alterIdx', port=conn.port)
+            client.redis.flushdb()
+
+            # Creating the index definition and schema
+            client.create_index((TextField('title'),))
+
+            # Using alter to add a field
+            client.alter_schema_add((TextField('body'),))
+
+            # Indexing a document
+            client.add_document('doc1', title = 'MyTitle', body = 'Some content only in the body')
+
+            # Searching with parameter only in the body (the added field)
+            q = Query("only in the body")
+
+            # Ensure we find the result searching on the added body field
+            res = client.search(q)
+            self.assertEqual(1, res.total)
+
+
 if __name__ == '__main__':
 
     unittest.main()
