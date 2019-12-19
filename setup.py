815fbfb0714151cb6620ad42735fcca23210e2c1
Guy Korland <gkorland@gmail.com>
2019-12-19 18:06:10 +0200

    add long_description (#49)

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import io
 
+def read_all(f):
+    with io.open(f, encoding="utf-8") as I:
+        return I.read()
+
+requirements = map(str.strip, open("requirements.txt").readlines())
 
 setup(
     name='redisearch',
     version='0.8.0',
-
     description='RedisSearch Python Client',
+    long_description=read_all("README.md"),
+    long_description_content_type='text/markdown',
     url='http://github.com/RedisLabs/redisearch-py',
     packages=find_packages(),
     install_requires=['redis', 'hiredis', 'rmtest'],
@@ -18,5 +25,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Database',
         'Topic :: Software Development :: Testing'
-    ]
+    ],
+    keywords='Redis Search Extension',
+    author='RedisLabs',
+    author_email='oss@redislabs.com'    
 )
