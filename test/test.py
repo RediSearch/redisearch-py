14850e1250b88a3969c465ec36ebfb90fb0b75d4
AvitalFineRedis <avital.fine@redislabs.com>
2021-06-17 18:17:04 +0300

    remove import

--- test/test.py
+++ test/test.py
@@ -19,7 +19,6 @@ from redisearch.client import IndexType
 import redisearch.aggregation as aggregations
 import redisearch.reducers as reducers
 import rejson
-import json
 
 WILL_PLAY_TEXT = os.path.abspath(os.path.dirname(__file__)) + '/will_play_text.csv.bz2'
 
