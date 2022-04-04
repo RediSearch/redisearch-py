d68e72047b2a82773c334d8ec46e86409b8a483f
dvora-h <67596500+dvora-h@users.noreply.github.com>
2022-04-04 17:31:10 +0300

    add deprecation warning

--- redisearch/__init__.py
+++ redisearch/__init__.py
@@ -1,3 +1,4 @@
+from warnings import warn
 from .result import Result
 from .document import Document
 from .client import Client, NumericField, TextField, GeoField, TagField, IndexDefinition
@@ -6,3 +7,7 @@ from .aggregation import AggregateRequest, AggregateResult
 from .auto_complete import AutoCompleter, Suggestion
 
 
+warnings.warn("As of redis-py 4.0.0 this library is deprecated. "
+"It's features have been merged into redis-py. "
+"Please either install it from pypi or the repo."
+, DeprecationWarning, stacklevel=2)
